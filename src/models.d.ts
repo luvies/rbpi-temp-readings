92155ade13fa6198642e9aac77dd58475ff4b350
Luke Davies <lkdvs3@gmail.com>
2018-10-22 12:59:19 +0100

    Added missing models

--- src/models.d.ts
+++ src/models.d.ts
@@ -1,14 +1,27 @@
 declare namespace Models {
   export namespace Api {
+    interface ReadingBase {
+      value: number;
+      takenAt: string;
+    }
+
     interface SensorBase {
       serialNum: string;
       desc: string;
     }
 
+    export namespace Get {
+      export interface Reading extends ReadingBase {
+        id: number;
+      }
+
+      export interface Sensor extends SensorBase {
+        id: number;
+      }
+    }
+
     export namespace Post {
-      export interface Reading {
-        value: number;
-        takenAt: string;
+      export interface Reading extends ReadingBase {
       }
 
       export interface Sensor extends SensorBase {
